ed6075526abc1da5162fd7ea3489e0a3c6b27ed8
xiaoshen <xiaoshen@eridani.club>
2021-12-01 00:25:47 -0600

    not too hard

--- src/main/kotlin/day1/day1.kt
+++ src/main/kotlin/day1/day1.kt
@@ -7,6 +7,13 @@ fun main() {
     readInputs(1)
         .map { it.toInt() }
         .windowed(2)
-        .count { (x , y) -> y > x }
+        .count { (x, y) -> y > x }
+        .println()
+
+    readInputs(1)
+        .map { it.toInt() }
+        .windowed(3) { it.sum() }
+        .windowed(2)
+        .count { (x, y) -> y > x }
         .println()
 }
